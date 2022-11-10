49d9b1a454133360cdd74e3555c2a0fd432a2b54
yasindegerV <yasin.deger@virgosol.com>
2022-11-10 10:30:42 +0300

    first

---
+++ Test Suites/TS_RegressionTest.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_RegressionTest</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e7b04e11-b6a8-4df5-a1f4-71cbb427510e</testSuiteGuid>
+</TestSuiteEntity>
