fd00dc7d2879572bc9a4382ad4a7d88e256303e8
yasindegerV <yasin.deger@virgosol.com>
2022-11-17 11:57:29 +0300

    docker files

---
+++ Test Suites/Docker_Regression_Test.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Docker_Regression_Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b646d996-9cb0-4803-a74c-24f0e636fa0c</testSuiteGuid>
+</TestSuiteEntity>
