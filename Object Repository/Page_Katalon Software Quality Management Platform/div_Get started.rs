79af5cdf14cfda7711746e56da215cf1907b1503
yasindegerV <yasin.deger@virgosol.com>
2022-11-17 11:56:40 +0300

    docker test uploaded

---
+++ Object Repository/Page_Katalon Software Quality Management Platform/div_Get started.rs
@@ -0,0 +1,129 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Get started</name>
+   <tag></tag>
+   <elementGuidId>028a9dd1-5bb7-4f4d-9051-040e3460f037</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='hs_cos_wrapper_module_85511515182']/section/div[2]/div</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.btn_get_started.trackable</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>bcede19b-0680-4e11-8020-0ec8e70041b7</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>btn_get_started trackable</value>
+      <webElementGuid>be476129-88a9-4dab-b759-15c20da7d19a</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-event-name</name>
+      <type>Main</type>
+      <value>Get started header Button Clicked</value>
+      <webElementGuid>13122a57-c3b9-4507-b08e-be65a242bbc3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-event-category</name>
+      <type>Main</type>
+      <value>User acquisition</value>
+      <webElementGuid>a16fe821-380f-4042-92d1-0b68f538cf59</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-event-label</name>
+      <type>Main</type>
+      <value>New Platfrom Home Page Sep 2022</value>
+      <webElementGuid>bf5565ee-9086-47a4-ab77-5a21e33b8263</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+            Get started
+        </value>
+      <webElementGuid>280d1964-6016-4b9d-b557-44db8a0ccdd2</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;hs_cos_wrapper_module_85511515182&quot;)/section[@class=&quot;hero_section&quot;]/div[@class=&quot;hero_btn&quot;]/div[@class=&quot;btn_get_started trackable&quot;]</value>
+      <webElementGuid>66b80851-d3bf-4fc1-91ac-539744a58288</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='hs_cos_wrapper_module_85511515182']/section/div[2]/div</value>
+      <webElementGuid>c5699d11-56fe-4afd-8c84-f31adc0c07f0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='quality management platform'])[1]/following::div[3]</value>
+      <webElementGuid>8a6b9a64-b4f8-4f65-9f98-2ed067442d84</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='View a demo'])[1]/preceding::div[1]</value>
+      <webElementGuid>365e4b21-3c4e-4080-9352-0ec72819f69c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Get started']/parent::*</value>
+      <webElementGuid>d253961c-6385-4d0e-a1f7-bf81d1d673e2</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//section/div[2]/div</value>
+      <webElementGuid>3f566c68-5348-4462-97ca-760696213e5b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '
+            Get started
+        ' or . = '
+            Get started
+        ')]</value>
+      <webElementGuid>4250e1a4-b5bf-4831-9347-818e40470aeb</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Page_Sign up  Katalon Solution/div_Create a Katalon account.rs
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Create a Katalon account</name>
+   <tag></tag>
+   <elementGuidId>319a1504-7db6-4783-841b-49ff90674fdd</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='hs_cos_wrapper_module_85550910293']/div[3]/div/div[2]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.form_title</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>eab29cb4-3f2a-411f-9855-57e77d39d8d6</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>form_title</value>
+      <webElementGuid>3132891c-eddc-4ce8-bd04-1744b34a3a2d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Create a Katalon account</value>
+      <webElementGuid>afeda0d6-92fd-4fc1-ab3f-7666b8400bed</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;hs_cos_wrapper_module_85550910293&quot;)/div[@class=&quot;sign_up_section&quot;]/div[@class=&quot;sign_up_form&quot;]/div[@class=&quot;form_title&quot;]</value>
+      <webElementGuid>04e409f9-bfe4-4afa-8ddc-c189fd959300</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='hs_cos_wrapper_module_85550910293']/div[3]/div/div[2]</value>
+      <webElementGuid>e4d0ad5a-7409-4f26-ba52-75daa0fbdfc4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Sign in'])[1]/following::div[4]</value>
+      <webElementGuid>5425bacb-9aae-4c72-9c6c-047c8c8ff65f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Already have an account?'])[1]/following::div[5]</value>
+      <webElementGuid>06ef6dd2-5b07-4cff-991b-62f79955059a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Valid email is required to activate products'])[1]/preceding::div[3]</value>
+      <webElementGuid>538d382a-4e6e-425c-891b-12f680551082</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Password Requirements'])[1]/preceding::div[5]</value>
+      <webElementGuid>d5968002-2011-4935-8ca1-c7c0d73b0fab</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Create a Katalon account']/parent::*</value>
+      <webElementGuid>22e3fed7-fbc9-4114-bef3-e9523f686740</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]/div/div[2]</value>
+      <webElementGuid>c7e70dba-35de-4551-8789-1155d79f0dea</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'Create a Katalon account' or . = 'Create a Katalon account')]</value>
+      <webElementGuid>c8056b69-adc0-4eb9-a2ed-48d1f8423134</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
